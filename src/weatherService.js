b76e805a4ca4affd2ed90a4126aeadff36d99a29
Manh Quan <manhquan.05012002@gmail.com>
2022-11-14 17:52:55 +0700

    u

---
+++ src/weatherService.js
@@ -0,0 +1,39 @@
+// const API_KEY = "f6d42914db9337c2b9ee5025e0b8e518";
+const API_KEY = "f00c38e0279b7bc85480c3fe775d518c";
+
+const makeIconURL = (iconId) =>
+  `https://openweathermap.org/img/wn/${iconId}@2x.png`;
+
+const getFormattedWeatherData = async (city, units = "metric") => {
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`;
+
+  const data = await fetch(URL)
+    .then((res) => res.json())
+    .then((data) => data);
+
+  const {
+    weather,
+    main: { temp, feels_like, temp_min, temp_max, pressure, humidity },
+    wind: { speed },
+    sys: { country },
+    name,
+  } = data;
+
+  const { description, icon } = weather[0];
+
+  return {
+    description,
+    iconURL: makeIconURL(icon),
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    pressure,
+    humidity,
+    speed,
+    country,
+    name,
+  };
+};
+
+export { getFormattedWeatherData };
